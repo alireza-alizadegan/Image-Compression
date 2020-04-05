a3a4c4a8b1250063f0c12c49df81e16064fd8823
Alireza Alizadegan <a.r.alizadegan@gmail.com>
2020-04-04 20:10:50 -0700

    code

--- Image Compression.m
+++ Image Compression.m
@@ -10,25 +10,25 @@ y = double(imread('mandrill.tiff'));
 % partition image to M*M blocks and loop through blocks
 M = 2; 
 n = prod(size(y))/(3*M*M); 
-d = size(y,1);
+d = size(y, 1);
 c = 0;
-x=zeros(n,3*M*M);
+x=zeros(n, 3*M*M);
 for i=1:M:d
     for j=1:M:d
         c = c+1;
         % reshape each block to vector of dimension 3*M^2
-        x(c,:) = reshape(y(i:i+M-1,j:j+M-1,:),[1,M*M*3]);
+        x(c,:) = reshape(y(i:i+M-1, j:j+M-1,:), [1, M*M*3]);
     end
 end
 
 % initialize centroid location
-dprime = size(x,2);
+dprime = size(x, 2);
 rng(0);
 perm = randperm(n);
-m = x(perm(1:k),:); 
+m = x(perm(1:k), :); 
 
-map_old = zeros(n,1);
-map = ones(n,1);
+map_old = zeros(n, 1);
+map = ones(n, 1);
 
 % update centroid location
 iter = 1;
@@ -36,15 +36,16 @@ while norm(map - map_old)>0
     iter
     map_old = map;
     
-    dist = dist2(x,m);
-    [Min,map] = min(dist,[],2);
+    dist = dist2(x, m);
+    [Min,map] = min(dist, [], 2);
     
     w(iter) = 0;
     for l=1:k
         a = map-l;
         zeroInd = find(a==0);
-        m(l,:) = mean(x(zeroInd,:));
-        w(iter) = w(iter)+sum(sum((x(zeroInd,:)-repmat(m(l,:),[size(x(zeroInd,:),1) 1])).^2,2));
+        m(l, :) = mean(x(zeroInd, :));
+        w(iter) = w(iter)+sum(sum((x(zeroInd, :)-repmat(m(l, :),
+        [size(x(zeroInd, :), 1) 1])).^2, 2));
     end
     iter = iter+1;
 end
@@ -53,14 +54,14 @@ end
 for l=1:k
     a = map-l;
     zeroInd = find(a==0);
-    x1(zeroInd,:) = repmat(m(l,:),[numel(zeroInd) 1]);
+    x1(zeroInd, :) = repmat(m(l, :),[numel(zeroInd) 1]);
 end
 
-c=0;
+c = 0;
 for i=1:M:d
     for j=1:M:d
         c = c+1;
-        y1(i:i+M-1,j:j+M-1,:) = reshape(x1(c,:),[M M 3]);
+        y1(i:i+M-1, j:j+M-1, :) = reshape(x1(c, :), [M M 3]);
     end
 end
 
@@ -81,5 +82,5 @@ title('Original')
 
 % difference image
 figure;
-dy=y1-y;
+dy = y1-y;
 imagesc(dy)
